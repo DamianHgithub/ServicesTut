5236ad32bbee64855994da9ed32b17e760d4df8b
BlasQu <damianhs138@gmail.com>
2021-03-15 19:07:23 +0100

    Testing Service 1 command

--- app/src/main/java/com/example/internetservicestutorial/MainActivity.kt
+++ app/src/main/java/com/example/internetservicestutorial/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.internetservicestutorial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.internetservicestutorial.di.DIManager
+import com.example.internetservicestutorial.util.TutService
 import org.koin.dsl.module
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, TutService::class.java)
+        intent.putExtra("testString", "siema")
+
+        startService(intent)
     }
 }
--- app/src/main/java/com/example/internetservicestutorial/util/TutService.kt
+++ app/src/main/java/com/example/internetservicestutorial/util/TutService.kt
@@ -3,11 +3,13 @@ package com.example.internetservicestutorial.util
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class TutService: Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+        Log.d("DEBUG", intent!!.getStringExtra("testString")!!)
+        return START_NOT_STICKY
     }
 
     override fun onBind(intent: Intent?): IBinder? {
