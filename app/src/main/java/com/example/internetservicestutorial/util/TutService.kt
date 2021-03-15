f06a3d92232b6e7c98876036a8a62799b67261e4
BlasQu <damianhs138@gmail.com>
2021-03-15 15:37:33 +0100

    Create Service

---
+++ app/src/main/java/com/example/internetservicestutorial/util/TutService.kt
@@ -0,0 +1,16 @@
+package com.example.internetservicestutorial.util
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class TutService: Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
