87186194b285715a14b4799bfdac1b7db032e618
BlasQu <damianhs138@gmail.com>
2021-03-16 12:35:07 +0100

    Turning off service with stopself after delayed Handler

--- app/src/main/java/com/example/internetservicestutorial/util/TutService.kt
+++ app/src/main/java/com/example/internetservicestutorial/util/TutService.kt
@@ -3,15 +3,22 @@ package com.example.internetservicestutorial.util
 import android.app.Notification
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import com.example.internetservicestutorial.di.DIManager
+import kotlinx.coroutines.*
 
 class TutService: Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("DEBUG", intent!!.getStringExtra("testString")!!)
+        Handler(Looper.myLooper()!!).postDelayed({
+            Toast.makeText(applicationContext, "show", Toast.LENGTH_SHORT).show()
+            stopSelf()
+        },5000)
 
         val mNotification = NotificationCompat.Builder(this, DIManager.channelID)
             .setContentTitle("Siema")
